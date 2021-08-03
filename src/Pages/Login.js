15dd3798ce4c298ea82c81e4dfd9ef591fdc0f5a
bohyunkang <bbohyun@kakao.com>
2021-08-04 00:15:56 +0900

    [#10, #12] feat : 로그인, 회원가입 페이지 유효성 검사 기능
    Co-authored-by: daeun-react <daeun-react@users.noreply.github.com>
    Co-authored-by: imhjlov <imhjlov@users.noreply.github.com>

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,14 +1,18 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Input from "Components/common/Input";
+import Button from "Components/common/Button";
 import { ReactComponent as Mail } from "Assets/svg/mail.svg";
 import { ReactComponent as ClosedEye } from "Assets/svg/eye_closed.svg";
 
 const Login = () => {
-  const [validate, setValidate] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({
+    email: { error: false, message: null },
+    password: { error: false, message: null },
+  });
 
   const onChangeEmail = useCallback((e) => {
     setEmail(e.target.value);
@@ -18,12 +22,38 @@ const Login = () => {
     setPassword(e.target.value);
   }, []);
 
+  const isEmail = (checkString) => {
+    const emailRegex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    return emailRegex.test(checkString);
+  };
+
+  const validateCheck = () => {
+    if (!email || !isEmail(email)) {
+      setErrors({
+        ...errors,
+        email: { error: true, message: "이메일을 다시 확인해주세요" },
+      });
+    }
+
+    if (!password) {
+      setErrors({
+        ...errors,
+        password: { error: true, message: "패스워드를 다시 확인해주세요" },
+      });
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setValidate(true);
-    //* email, password validation 성공하면 false로 수정
-    // setValidate(false);
+    validateCheck();
+    //*로컬스토리지에서 id, pw 일치하는 사람 있는지
+    //if
   };
+
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
 
   return (
     <Wrapper>
@@ -34,8 +64,8 @@ const Login = () => {
           onChange={onChangeEmail}
           placeholder="이메일을 입력하세요"
           icon={<Mail />}
-          error={validate && !email}
-          errorMessage="이메일을 입력하세요"
+          error={errors.email.error}
+          errorMessage={errors.email.message}
         />
 
         <Input
@@ -45,15 +75,11 @@ const Login = () => {
           onChange={onChangePassword}
           placeholder="비밀번호를 입력하세요"
           icon={<ClosedEye />}
-          error={validate && !password}
-          errorMessage="비밀번호를 입력하세요"
+          error={errors.password.error}
+          errorMessage={errors.password.message}
         />
 
-        <button
-          type="submit"
-          style={{ height: "50px", backgroundColor: "#78e08f", width: "100%", color: "white" }}>
-          로그인
-        </button>
+        <Button type="submit" value="로그인" marginTop="10px" />
       </Form>
       <NavLink to="/signup">
         <p>
