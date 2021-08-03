dbde2d6e13e1e2a0911f72db53a60791ee893c02
daeun-react <67173064+daeun-react@users.noreply.github.com>
2021-08-03 20:23:42 +0900

    Revert "로그인 페이지 마크업 및 Input 컴포넌트 머지"

--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -1,9 +1,7 @@
 export const Theme = {
   color: {
     green: "#87BF44",
-    blue: "#0984e3",
     red: "#e55039",
-    lightred: "#FFF8F6",
     button: "#78e08f",
     buttonHover: "#b8e994",
     inputBackground: "#F9F9F9",
