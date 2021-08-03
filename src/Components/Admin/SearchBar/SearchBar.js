6779db69863fe1cb59f5bc4042c8a52d124b016b
Namju <skawn11114@gmail.com>
2021-08-03 14:59:38 +0900

    [#9] feat : 검색바 마크업 및 이벤트핸들러 적용

--- src/Components/Admin/SearchBar/SearchBar.js
+++ src/Components/Admin/SearchBar/SearchBar.js
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 
 const SearchBar = () => {
-  return <Adiv>SearchBar</Adiv>;
+  const handleSearchClick = () => {
+    //console.log(searchKeyword.current.value);
+  };
+  const handleSearchKind = (e) => {
+    // console.log(e.target.value);
+  };
+  const searchKeyword = useRef("");
+
+  return (
+    <Wrapper>
+      <div className="searchBar">
+        <select onChange={handleSearchKind}>
+          <option value="email">Email</option>
+          <option value="name">Name</option>
+        </select>
+        <input className="searchInput" placeholder="검색 입력" ref={searchKeyword}></input>
+        <button className="searchButton" onClick={handleSearchClick}>
+          검색
+        </button>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SearchBar;
 
-const Adiv = styled.div`
-  background-color: red;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .searchBar {
+    display: flex;
+    min-width: 470px;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10px;
+    border: 1px solid ${({ theme }) => theme.color.borderline};
+    border-radius: 30px;
+    background-color: ${({ theme }) => theme.color.inputBackground};
+    & > select {
+      background-color: ${({ theme }) => theme.color.inputBackground};
+      border: 1px solid ${({ theme }) => theme.color.borderline};
+      border-radius: 10px;
+    }
+
+    .searchInput {
+      height: 50px;
+      width: 300px;
+      margin: 0 10px;
+    }
+
+    button {
+      border-radius: 10px;
+      background-color: ${({ theme }) => theme.color.button};
+      color: ${({ theme }) => theme.color.fontWhite};
+    }
+  }
 `;
