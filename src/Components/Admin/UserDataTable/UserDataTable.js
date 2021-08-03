470447adb6c7424b5366df378db69d8519603661
hyunchanpark <eye3570@gmail.com>
2021-08-03 15:57:23 +0900

    [#11] feat : User Mock Data를 UserDataTable에 적용
    Co-authored-by: SeongsangCHO <SeongsangCHO@users.noreply.github.com>
    Co-authored-by: junamee <skawnkk@users.noreply.github.com>

--- src/Components/Admin/UserDataTable/UserDataTable.js
+++ src/Components/Admin/UserDataTable/UserDataTable.js
@@ -1,12 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 
-const UserDataTable = () => {
-  const [user, setUser] = useState([]);
-  useEffect(() => {
-    // 사용자 Mock 데이터 가져오기
-  }, []);
-
+const UserDataTable = ({ users }) => {
   return (
     <Wrapper>
       <div className="dataTable">
@@ -14,65 +9,45 @@ const UserDataTable = () => {
           <tbody>
             <tr>
               <th>id</th>
-              <th>pw</th>
               <th>이름</th>
               <th>이메일</th>
+              <th>pw</th>
               <th>주소</th>
               <th>생년월일</th>
               <th>신용 카드</th>
               <th colSpan="3">권한</th>
-              <th></th>
-              <th></th>
             </tr>
-            <tr>
-              <td>1</td>
-              <td>alalal</td>
-              <td>넝담곰</td>
-              <td>1fff</td>
-              <td>1sss</td>
-              <td>1sss</td>
-              <td>1sss</td>
-
-              <td>1sss</td>
-              <td>
-                <select>
-                  <option value="">1</option>
-                  <option value="">2</option>
-                  <option value="">3</option>
-                </select>
-              </td>
-              <td>
-                <button>권한수정</button>
-              </td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>alalal</td>
-              <td>넝담곰</td>
-              <td>1fff</td>
-              <td>1sss</td>
-              <td>1sss</td>
-              <td>1sss</td>
-
-              <td>1sss</td>
-              <td>
-                <select>
-                  <option value=""></option>
-                  <option value=""></option>
-                  <option value=""></option>
-                </select>
-              </td>
-              <td>
-                <button>권한수정</button>
-              </td>
-            </tr>
+            {users.map(
+              ({ id, name, email, pw, address, dateOfBirth, creditCardNum, authority }) => (
+                <tr>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{email}</td>
+                  <td>{pw}</td>
+                  <td>{address}</td>
+                  <td>{dateOfBirth}</td>
+                  <td>{creditCardNum}</td>
+                  <td>{authority}</td>
+                  <td>
+                    <select>
+                      <option value="">1</option>
+                      <option value="">2</option>
+                      <option value="">3</option>
+                    </select>
+                  </td>
+                  <td>
+                    <button>권한수정</button>
+                  </td>
+                </tr>
+              )
+            )}
           </tbody>
         </table>
       </div>
     </Wrapper>
   );
 };
-// id pw name email address dateofbirth creditCardnum authority
+
 export default UserDataTable;
 
 const Wrapper = styled.div`
--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "Components/Admin/SearchBar/SearchBar";
 import AuthFilter from "Components/Admin/AuthFilter/AuthFilter";
 import UserDataTable from "Components/Admin/UserDataTable/UserDataTable";
 import Pagination from "Components/Admin/Pagination/Pagination";
+import { userMockData } from "Utils/MockData";
 
 const Admin = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    setUsers(userMockData); // url: /admin/pageNumber
+  }, []);
+
   return (
     <div>
       {/* <Header /> */}
@@ -12,7 +19,7 @@ const Admin = () => {
       <div>사용자 관리</div>
       <SearchBar />
       <AuthFilter />
-      <UserDataTable />
+      <UserDataTable users={users} />
       <Pagination />
     </div>
   );
