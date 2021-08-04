2acdb3cd03caeb64da64f924b384d91102861ce9
Seongsangcho <liasqui31@gmail.com>
2021-08-05 02:20:55 +0900

    [#4] style : 페이지네이션 버튼 글자 수정
    Co-authored-by: junamee <skawnkk@users.noreply.github.com>
    Co-authored-by: Eye0n <Eyes0n@users.noreply.github.com>

--- src/Components/Admin/Pagination/Pagination.js
+++ src/Components/Admin/Pagination/Pagination.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-//TODO: 상수 관리
-import { PAGE_SIZE } from "Pages/Admin";
 
 const Pagination = ({ pageNum, setPageNum, wholePages }) => {
+  // const viewPageCount = wholePages / 10; 0이면 1~10인데, wholePage === 전체 페이지 갯수.
+  // const totalPages = Array(wholePages % 10).fill(0).map((_, idx) => pageNum + idx))
+  //현재페이지 pageNum 10, => 페이지사이즈 10 next클릭시 => 11, 20일때 next클릭시 21
+  // pageNum % 10 === 0 일때, totalPages [11,12,13,,,,,20]
+  // pageNum % PAGE_SIZE === 0 일때, [10에서 11을 눌렀을 때]=>
   const totalPages = Array.from({ length: wholePages }, (_, idx) => idx + 1);
+
   const handlePage = (e, page) => {
     const type = e.target.id;
     if (type === "prev") setPageNum((prevPage) => prevPage - 1);
@@ -17,10 +21,10 @@ const Pagination = ({ pageNum, setPageNum, wholePages }) => {
   return (
     <PaginationWrapper>
       <PaginationBtn id="first" onClick={handlePage} pageNum={pageNum} disabled={!pageNum}>
-        {`<<`}처음
+        {`<< `}First
       </PaginationBtn>
       <PaginationBtn id="prev" onClick={handlePage} pageNum={pageNum} disabled={!pageNum}>
-        {`<`}PREV
+        {`< `}Prev
       </PaginationBtn>
       {totalPages.map((page, idx) => (
         <PaginationBtn
@@ -37,23 +41,24 @@ const Pagination = ({ pageNum, setPageNum, wholePages }) => {
         onClick={handlePage}
         pageNum={pageNum}
         disabled={pageNum === wholePages - 1}>
-        NEXT {`>`}
+        Next {` >`}
       </PaginationBtn>
       <PaginationBtn
         id="last"
         onClick={handlePage}
         pageNum={pageNum}
         disabled={pageNum === wholePages - 1}>
-        END {`>>`}
+        End {` >>`}
       </PaginationBtn>
     </PaginationWrapper>
   );
 };
 
-export default Pagination;
+export default React.memo(Pagination);
 
 const PaginationWrapper = styled.div`
   ${({ theme }) => theme.flexSet()};
+  margin-top: 15px;
 `;
 
 const PaginationBtn = styled.button`
