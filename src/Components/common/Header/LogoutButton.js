12475d522858c70e33a1555734d52cabbeb50bf0
bohyunkang <bbohyun@kakao.com>
2021-08-05 18:06:40 +0900

    [#28] Feat : Header 드롭다운 시 로그인 유저 아이디 추가

--- src/Components/common/Header/LogoutButton.js
+++ src/Components/common/Header/LogoutButton.js
@@ -6,13 +6,20 @@ import { loadLocalStorage, loggedOutStorage } from "Utils/Storage";
 import profile from "Assets/img/profile.png";
 
 const LogoutButton = () => {
-  const { name } = loadLocalStorage(LOGGEDIN_USER)[0];
+  const userData = loadLocalStorage(LOGGEDIN_USER);
   const dropdownRef = useRef(null);
   const [activeMenu, setActiveMenu] = useState(false);
   const onClick = () => setActiveMenu(!activeMenu);
 
   const handleLogout = () => {
     loggedOutStorage();
+  };
+
+  const getUserName = () => {
+    if (userData) {
+      const { name } = userData[0];
+      return name;
+    }
   };
 
   useEffect(() => {
@@ -39,7 +46,7 @@ const LogoutButton = () => {
         </div>
         <ul ref={dropdownRef} className={`menu ${activeMenu ? "menu-active" : ""}`}>
           <li>
-            <Link to="/">{name}</Link>
+            <Link to="/">{getUserName()}</Link>
           </li>
           <li>
             <Link to="/">내 계정 관리</Link>
